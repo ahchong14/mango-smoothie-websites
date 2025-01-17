2e7408c93038a49839bb304caa9de77074873222
ahchong <bashenkuok6801@gamil,com>
2025-01-18 01:38:29 +0800

    Convert index.js to TypeScript

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>Welcome to Mango Smoothie</h1>
-      <p>Your healthy drink destination</p>
-    </div>
-  )
-} 
